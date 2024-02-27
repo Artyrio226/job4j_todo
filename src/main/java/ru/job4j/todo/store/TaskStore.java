a3b779f62e496f84623a2864b3d97ff0a41fcc5a
Artyrio226 <artur_sar_80@mail.ru>
2024-02-27 18:48:53 +0300

    4. TODO list [#3786] добавлены класс Main, класс конфигурации, слой персистенции store

---
+++ src/main/java/ru/job4j/todo/store/TaskStore.java
@@ -0,0 +1,113 @@
+package ru.job4j.todo.store;
+
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+import ru.job4j.todo.model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@AllArgsConstructor
+public class TaskStore implements ITaskStore {
+    private final SessionFactory sf;
+
+    @Override
+    public Task save(Task task) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.save(task);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return task;
+    }
+
+    @Override
+    public boolean update(Task task) {
+        boolean rsl = false;
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            rsl = session.createQuery("""
+                                     UPDATE Task
+                                      SET title = :fTitle, description = :fDescription,
+                                      created = :fCreated, done = :fDone
+                                      WHERE id = :fId
+                                     """)
+                    .setParameter("fTitle", task.getTitle())
+                    .setParameter("fDescription", task.getDescription())
+                    .setParameter("fCreated", task.getCreated())
+                    .setParameter("fDone", task.isDone())
+                    .setParameter("fId", task.getId())
+                    .executeUpdate() > 0;
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return rsl;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        boolean rsl = false;
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            rsl = session.createQuery("DELETE Task WHERE id = :fId")
+                    .setParameter("fId", id)
+                    .executeUpdate() > 0;
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return rsl;
+    }
+
+    @Override
+    public List<Task> findAll() {
+        Session session = sf.openSession();
+        List<Task> list = new ArrayList<>();
+        try {
+            session.beginTransaction();
+            list = session.createQuery("from Task", Task.class)
+                    .getResultList();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return list;
+    }
+
+    @Override
+    public Optional<Task> findById(int id) {
+        Session session = sf.openSession();
+        Optional<Task> optionalTask = Optional.empty();
+        try {
+            session.beginTransaction();
+            Query<Task> query = session.createQuery("from Task where id = :fId", Task.class)
+                    .setParameter("fId", id);
+            optionalTask = query.uniqueResultOptional();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return optionalTask;
+    }
+}
